31e1f4c86ff3edc59af34673ec837d48d62d74a2
azu-caylent <charles.ikenyei@caylent.com>
2022-07-18 13:46:49 -0400

    fix: update bug3

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi there, %v. Welcome!", name)
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
 }
 
