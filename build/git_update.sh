2788bf171e77d2233072a085f150a4ab42bc0461
azu-caylent <charles.ikenyei@caylent.com>
2022-07-22 01:32:08 -0400

    Testing v4.0.0

--- build/git_update.sh
+++ build/git_update.sh
@@ -16,7 +16,7 @@ CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  CURRENT_VERSION='v3.0.0'
+  CURRENT_VERSION='v4.0.0'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
