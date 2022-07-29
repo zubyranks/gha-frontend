d9e853c1cc3bdc6c09f7647aef6dad20a48097d4
azu-caylent <charles.ikenyei@caylent.com>
2022-07-29 13:46:26 -0400

    Changed to v1.26.17

--- build/git_update.sh
+++ build/git_update.sh
@@ -14,7 +14,7 @@ done
 git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
-if [[ $CURRENT_VERSION == 'v1.26.16' ]]
+if [[ $CURRENT_VERSION == 'v1.26.15' ]]
 then
   CURRENT_VERSION='v1.26.17'
 fi
