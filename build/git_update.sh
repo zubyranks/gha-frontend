1d1e59f676a3490ac60a3d9444aa90f0641601b9
zubyranks <zubyranks@gmail.com>
2022-09-12 11:22:06 -0400

    Update git_update.sh

--- build/git_update.sh
+++ build/git_update.sh
@@ -14,9 +14,9 @@ done
 git fetch --prune --unshallow 2>/dev/null
 CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
-if [[ $CURRENT_VERSION == '' ]]
+if [[ $CURRENT_VERSION == 'v1.26.32' ]]
 then
-  CURRENT_VERSION='x.x.x'
+  CURRENT_VERSION='v1.26.33'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
